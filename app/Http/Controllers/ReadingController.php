6375854f7492db0d7db1e6cf1845ff367beb8191
Mark Gabinete <81798034+markgabinete02@users.noreply.github.com>
2022-10-21 08:25:04 +0800

    update reading

--- app/Http/Controllers/ReadingController.php
+++ app/Http/Controllers/ReadingController.php
@@ -16,20 +16,22 @@ class ReadingController extends Controller
 
         $price = Utility::find(1);
 
+        $new_current = $request->input('reading') + $account->current_reading;
+
         $reading_diff = $request->input('reading') - $account->current_reading;
 
         $payment = $reading_diff * $price->value;
 
         $account->prev_reading = $account->current_reading;
-        $account->current_reading = $request->input('reading');
+        $account->current_reading = $new_current;
         $account->current_charges = $payment;
         $account->update();
 
         $reading = new Reading;
         $reading->client_id = $request->input('client_id');
         $reading->meterman_id = auth()->user()->id;
-        $reading->prev_reading = $request->input('reading') - $reading_diff;
-        $reading->current_reading = $request->input('reading');
+        $reading->prev_reading = $new_current - $reading_diff;
+        $reading->current_reading = $new_current;
         $reading->price = $payment;
         $reading->save();
 
