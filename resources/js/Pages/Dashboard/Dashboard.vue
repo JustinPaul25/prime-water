4f27aa586e5b81862cbb0897c43db969c362b7db
Mark Gabinete <justinpaul1025@gmail.com>
2022-12-09 07:30:45 +0800

    update

--- resources/js/Pages/Dashboard/Dashboard.vue
+++ resources/js/Pages/Dashboard/Dashboard.vue
@@ -52,23 +52,23 @@ onMounted(() => populateData())
                             <dl class="rounded-lg bg-white shadow-lg sm:grid sm:grid-cols-5">
                                 <div class="flex flex-col border-b border-gray-100 p-6 text-center sm:border-0 sm:border-r">
                                     <dt class="order-2 mt-2 text-sm font-medium leading-6 text-gray-500">Barangay Population</dt>
-                                    <dd class="order-1 text-4xl font-bold tracking-tight text-primary-blue">1509</dd>
+                                    <dd class="order-1 text-4xl font-bold tracking-tight text-primary-blue">1968</dd>
                                 </div>
                                 <div class="flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r">
-                                    <dt class="order-2 mt-2 text-sm font-medium leading-6 text-gray-500">Screened Individuals</dt>
-                                    <dd class="order-1 text-4xl font-bold tracking-tight text-primary-blue">1509</dd>
+                                    <dt class="order-2 mt-2 text-sm font-medium leading-6 text-gray-500">No. of Puroks</dt>
+                                    <dd class="order-1 text-4xl font-bold tracking-tight text-primary-blue">5</dd>
                                 </div>
                                 <div class="flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r">
-                                    <dt class="order-2 mt-2 text-sm font-medium leading-6 text-gray-500">Number of Households</dt>
-                                    <dd class="order-1 text-4xl font-bold tracking-tight text-primary-blue">272</dd>
+                                    <dt class="order-2 mt-2 text-sm font-medium leading-6 text-gray-500">Households</dt>
+                                    <dd class="order-1 text-4xl font-bold tracking-tight text-primary-blue">463</dd>
                                 </div>
                                 <div class="flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r">
-                                    <dt class="order-2 mt-2 text-sm font-medium leading-6 text-gray-500">Availed Household</dt>
+                                    <dt class="order-2 mt-2 text-sm font-medium leading-6 text-gray-500">Availed Households</dt>
                                     <dd class="order-1 text-4xl font-bold tracking-tight text-primary-blue">272</dd>
                                 </div>
                                 <div class="flex flex-col border-t border-gray-100 p-6 text-center sm:border-0 sm:border-l">
-                                    <dt class="order-2 mt-2 text-sm font-medium leading-6 text-gray-500">Adjusted Individuals</dt>
-                                    <dd class="order-1 text-4xl font-bold tracking-tight text-primary-blue">50</dd>
+                                    <dt class="order-2 mt-2 text-sm font-medium leading-6 text-gray-500">In-Active Households</dt>
+                                    <dd class="order-1 text-4xl font-bold tracking-tight text-primary-blue">191</dd>
                                 </div>
                             </dl>
                         </div>
@@ -78,7 +78,7 @@ onMounted(() => populateData())
             <div class="bg-white">
                 <div class="grid overflow-hidden w-full px-8 lg:px-40">
                     <div class="box row-span-2 p-5">
-                        <p class="order-1 text-4xl font-bold tracking-tight text-primary-blue">2022 Income Chart</p>
+                        <p class="order-1 text-4xl font-bold tracking-tight text-primary-blue">Forecast Chart</p>
                         <income-chart class="h-60" v-if="datas.length != 0" :raw-data="datas" :result="getResult()"></income-chart>
                     </div>
                 </div>
