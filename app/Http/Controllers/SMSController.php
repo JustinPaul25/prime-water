844ad062a850edd181045bd01744ae8290a4fcb7
Mark Gabinete <justinpaul1025@gmail.com>
2023-02-13 13:28:10 +0800

    update

--- app/Http/Controllers/SMSController.php
+++ app/Http/Controllers/SMSController.php
@@ -21,8 +21,8 @@ class SMSController extends Controller
     public function notify(User $user)
     {
         $config = Configuration::getDefaultConfiguration()
-            ->setUsername('rocesregine@gmail.com')
-            ->setPassword('5F95AAAB-35F7-9FAC-95A9-50C6FE112556');
+            ->setUsername('regineroces')
+            ->setPassword('RegineRoces@27');
 
         $apiInstance = new SMSApi(
             new GuzzleClient(),
@@ -50,8 +50,8 @@ class SMSController extends Controller
     public function bill(User $user)
     {
         $config = Configuration::getDefaultConfiguration()
-            ->setUsername('elmer.angcla@dnsc.edu.ph')
-            ->setPassword('1B05DB49-A364-907D-AB74-56AC71BC7476');
+            ->setUsername('regineroces')
+            ->setPassword('RegineRoces@27');
 
         $apiInstance = new SMSApi(
             new GuzzleClient(),
--- database/seeders/ClientSeeder.php
+++ database/seeders/ClientSeeder.php
@@ -115,5 +115,54 @@ class ClientSeeder extends Seeder
                 'updated_at' => $datePaid[$dateInd-1],
             ]);
         }
+
+        $dateInd = rand(1,4);
+
+        $addIndex = rand(0, 4);
+
+        $user = new User;
+        $user->username = $faker->name();
+        $user->first_name = $faker->firstName();
+        $user->last_name = $faker->lastName();
+        $user->name = $user->first_name . ' ' . $user->last_name;
+        $user->password = Hash::make('123123');
+        $user->address = $address[$addIndex];
+        $user->status = true;
+        $user->email_verified_at = Carbon::now();
+        $user->save();
+
+        $user->assignRole('Client');
+
+        $account = new Account;
+        $account->client_id = $user->id;
+        $account->prev_reading = 0;
+        $account->current_reading = 0;
+        $account->prev_balance = 0;
+        $account->current_charges = 0;
+        $account->last_payment = '2022-07-14 11:23:04';
+        $account->save();
+
+        $price = Utility::find(1);
+        $randReading = rand(10,200);
+
+        $reading_diff = $randReading - $account->current_reading;
+
+        $payment = $reading_diff * $price->value;
+
+        $account->prev_reading = $account->current_reading;
+        $account->current_reading = $randReading;
+        $account->current_charges = $payment;
+        $account->update();
+
+        $reading = new Reading;
+        $reading->client_id = $user->id;
+        $reading->meterman_id = 2;
+        $reading->prev_reading = $randReading - $reading_diff;
+        $reading->current_reading = $randReading;
+        $reading->cum_price = $price->value;
+        $reading->price = $payment;
+        $reading->updated_at = $date[1];
+        $reading->created_at = $date[1];
+        $reading->save(['timestamps' => false]);
     }
 }
