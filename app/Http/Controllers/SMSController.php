d1eedec26009919d6916c8a20c369e59c5e09d7f
Mark Gabinete <justinpaul1025@gmail.com>
2023-03-02 15:04:10 +0800

    update

--- app/Http/Controllers/SMSController.php
+++ app/Http/Controllers/SMSController.php
@@ -76,48 +76,21 @@ class SMSController extends Controller
         $balance = number_format($balance, 2);
         $string = $user->contact_no;
         $number = substr_replace($string, "+63", 0, 1);
+        $message = 'Hello '.$user->first_name.' '.$user->last_name.' Here is your bill this month and the due date will be on' . $date . '. Current Bill: ₱'.$balance;
 
-        $number = $user->contact_no;      $message = 'Hello '.$user->first_name.' '.$user->last_name.' Here is your bill this month and the due date will be on' . $date . '. Current Bill: ₱'.$balance;
+        $client = new Client(config('twilio.account_sid'), config('twilio.auth_token'));
 
-        $client = new Client();
-        $url = 'https://api.itexmo.com/api/broadcast';
-
-        $response = $client->request(
-            'POST',
-            $url,
+        $message = $client->messages->create(
+            $number,
             [
-                'json' => [
-                    'Email' => 'elmer.angcla@dnsc.edu.ph',
-                    'Password' => 'Qwerty123',
-                    "Recipients" => [ $number ],
-                    "Message" => $message,
-                    'ApiCode' => 'PR-ELMER566617_LK5KT'
-                ]
+                'from' => config('twilio.from'),
+                'body' => $message
             ]
         );
 
-        $result = $response->getBody()->getContents();
-        return $result;
-
-        if ($result == '0') {
-            return 'Message sent successfully!';
-        } else {
-            return 'Message sending failed.';
-        }  }
-    // public function notify(User $user)
-    // {
-    //     $message = 'Hello '.$user->first_name.' '.$user->last_name.' This is a notice of disconnetion from WBS. Current Bill: '.$user->account->curent_charges.'.';
-
-    //     // $basic  = new \Vonage\Client\Credentials\Basic("91feaea9", "VoyeRquAsliUvY6V");
-    //     // $client = new \Vonage\Client($basic);
-
-    //     // $response = $client->sms()->send(
-    //     //     new \Vonage\SMS\Message\SMS("639293194425", 'NEXMO', $message)
-    //     // );
-
-    //     $response = Http::get('http://MingSms.mingming13.repl.co?phone=' . '09934451453' . '&message=' . $message . '&key=wbs_system');
-    //     //$send = json_encode(file_get_contents('https://MingSms.mingming13.repl.co?phone=' . '09934451453' . '&message=' . $message . '&key=wbs_system'));
-
-    //     return $response;
-    // }
+        return response()->json([
+            'message' => 'SMS sent successfully.',
+            'data' => $message
+        ]);
+    }
 }
